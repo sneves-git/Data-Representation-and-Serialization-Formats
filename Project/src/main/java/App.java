07708dee3bf73761eab6d54d6f5dcbefdc6fc5ea
Sofia Neves <at.work.sneves@gmail.com>
2022-09-20 14:51:05 +0100

    Project upload
    - Addition of project skeleton

---
+++ Project/src/main/java/App.java
@@ -0,0 +1,50 @@
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+
+import java.io.File;
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        JAXBContext jaxbContext = null;
+        try {
+
+            jaxbContext = org.eclipse.persistence.jaxb.JAXBContextFactory
+                    .createContext(new Class[]{Student.class}, null);
+
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            // output pretty printed
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            Student s1 = new Student();
+            Student s2 = new Student();
+            Student s3 = new Student();
+            s1.setName("Alberto");
+            s1.setAge("21");
+            s2.setName("Patricia");
+            s2.setAge("22");
+            s3.setName("Luis");
+            s3.setAge("21");
+
+            List<Student> students = new ArrayList<Student>();
+            students.add(s1);
+            students.add(s2);
+            students.add(s3);
+
+            // output to a xml file
+            jaxbMarshaller.marshal(students, new File("students.xml"));
+
+            // output to console
+            jaxbMarshaller.marshal(students, System.out);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ Project/src/main/java/Student.java
@@ -0,0 +1,44 @@
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Student {
+
+    @XmlAttribute
+    int id;
+
+    @XmlElement(name = "name")
+    String name;
+
+    @XmlElement(name = "age")
+    String age;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return this.age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+}
